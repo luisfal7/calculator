614fd5a7a931a3964ac3190a0009a762907f5a79
Luis Falconi <luism.falconi@gmail.com>
2023-01-16 16:36:36 -0300

    add state

--- src/theme/appTheme.tsx
+++ src/theme/appTheme.tsx
@@ -13,7 +13,8 @@ export const styles = StyleSheet.create({
     resultado:{
         color:'white',
         fontSize: 60,
-        textAlign:'right'
+        textAlign:'right',
+        marginBottom: 10
     },
     resultadoSecundario:{
         color:'rgba(255,255,255,0.5)',
