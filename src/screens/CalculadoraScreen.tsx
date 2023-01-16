6d78f3cd0cd5cb3c64cf3d55e5ef8d98210a2a4e
Luis Falconi <luism.falconi@gmail.com>
2023-01-16 16:52:47 -0300

    add condiciones calc

--- src/screens/CalculadoraScreen.tsx
+++ src/screens/CalculadoraScreen.tsx
@@ -12,8 +12,37 @@ export const CalculadoraScreen = () => {
   };
 
   const armarNumero = (numeroTexto: string) => {
-    setNumero(numero + numeroTexto);
+    if(numero.includes('.') && numeroTexto === '.') return
+
+    if(numero.startsWith('0') || numero.startsWith('-0')){
+        //punto decimal
+        if(numeroTexto === '.'){
+            setNumero(numero + numeroTexto);
+
+            // Evaluar si es otro cero, y hay un punto
+        }else if(numeroTexto === '0' && numero.includes('.')){
+            setNumero(numero + numeroTexto)
+
+            // Evaluar si es diferente de cero y no tiene un punto
+        }else if(numeroTexto !== '0' && !numero.includes('.')){
+            setNumero(numeroTexto)
+
+            //Evitar 0000.0
+        }else if(numeroTexto === '0' && !numero.includes('.')){
+            setNumero(numero)
+        }else{
+            setNumero(numero + numeroTexto)
+        }
+    }
   };
+
+  const positivoNegativo = () => {
+    if(numero.includes('-')){
+        setNumero(numero.replace('-',''))
+    }else{
+        setNumero('-' + numero)
+    }
+  }
 
   return (
     <View style={styles.calculadoraContainer}>
@@ -25,7 +54,7 @@ export const CalculadoraScreen = () => {
       {/* fila de botones */}
       <View style={styles.fila}>
         <BotonCalc texto="C" color="#9B9B9B" accion={limpiar} />
-        <BotonCalc texto="+/-" color="#9B9B9B" accion={limpiar}/>
+        <BotonCalc texto="+/-" color="#9B9B9B" accion={positivoNegativo}/>
         <BotonCalc texto="del" color="#9B9B9B" accion={limpiar}/>
         <BotonCalc texto="/" color="#FF9427" accion={limpiar} />
       </View>
