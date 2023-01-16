b4d1045474b3ad13aef4970de6d04224816dddff
Luis Falconi <luism.falconi@gmail.com>
2023-01-16 14:27:58 -0300

    firt commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>
+        hola mundo
+      </Text>
+    </View>
+  )
+}
+
+export default App;
