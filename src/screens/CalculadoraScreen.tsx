bce0454794bf9bc3bea217753bb2289c7446edfd
Luis Falconi <luism.falconi@gmail.com>
2023-01-16 14:45:29 -0300

    add theme, comp CalculadoraScreen

---
+++ src/screens/CalculadoraScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import { styles } from '../theme/appTheme';
+
+export const CalculadoraScreen = () => {
+  return (
+    <View>
+        <Text style={styles.texto}>
+            Calculadora screen
+        </Text>
+    </View>
+  )
+}
