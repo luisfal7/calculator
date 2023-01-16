2178a6c639db51c8cdf3f866f80fb44e3c24f55d
Luis Falconi <luism.falconi@gmail.com>
2023-01-16 15:58:11 -0300

    add styles boton

--- src/screens/CalculadoraScreen.tsx
+++ src/screens/CalculadoraScreen.tsx
@@ -1,27 +1,23 @@
-import React from 'react'
-import { Text, View } from 'react-native'
-import { styles } from '../theme/appTheme';
+import React from 'react';
+import {Text, View} from 'react-native';
+import { BotonCalc } from '../components/BotonCalc';
+import {styles} from '../theme/appTheme';
 
 export const CalculadoraScreen = () => {
   return (
     <View style={styles.calculadoraContainer}>
-        <Text style={styles.resultadoSecundario}>
-            1,500.00
-        </Text>
-        <Text style={styles.resultado}>
-            1,500.00
-        </Text>
+      <Text style={styles.resultadoSecundario}>1,500.00</Text>
+      <Text style={styles.resultado}>1,500.00</Text>
 
-        <View>
+      <View style={styles.fila}>
 
-            <View style={styles.boton}>
-                <Text style={styles.botonTexto}>
-                    1
-                </Text>
-            </View>
+        <BotonCalc texto="C" color='#9B9B9B'/>
+        <BotonCalc texto="+/-" color='#9B9B9B'/>
+        <BotonCalc texto="del" color='#9B9B9B'/>
+        <BotonCalc texto="/" color="#FF9427"/>
 
-        </View>
+      </View>
 
     </View>
-  )
-}
+  );
+};
